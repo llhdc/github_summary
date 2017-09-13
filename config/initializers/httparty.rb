548ccd2862b5f6c0521d83adade1610a36dcfab6
Leonard <leonard.horne@gmail.com>
2017-09-13 12:06:02 -0400

    Comment out amusing but sabotaging code.

--- config/initializers/httparty.rb
+++ config/initializers/httparty.rb
@@ -19,7 +19,7 @@ class Net::HTTPRequest
   # ░░▀▀▀▀░░░░░░░░░░░░░░░░░░░░░░█▄▄▄▄▄▄▄▄▄██
   # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
   def initialize(path, initheader = nil)
-    sleep rand(1..7) # muhahaha
+    # sleep rand(1..7) # muhahaha
     super self.class::METHOD,
           self.class::REQUEST_HAS_BODY,
           self.class::RESPONSE_HAS_BODY,
